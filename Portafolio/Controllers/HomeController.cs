3d467db6ece3e96146366dbfa4ebec4df6147bca
Rafael Jimenez <125316019+rjmoreno2022@users.noreply.github.com>
2024-02-14 21:47:34 -0400

    54. Listado de Proyectos

--- Portafolio/Controllers/HomeController.cs
+++ Portafolio/Controllers/HomeController.cs
@@ -15,7 +15,42 @@ namespace Portafolio.Controllers
 
         public IActionResult Index()
         {
+            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View();
+        }
+
+        private List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>() {  new Proyecto
+            {
+                Titulo = "Amazon",
+                Description = "E-Commerce realizado en ASP-NET Core",
+                Link = "https://www.amazon.com/",
+                ImagenURL = "/imagenes/amazon.png"
+            },
+            new Proyecto
+            {
+                Titulo = "New York Times",
+                Description = "Pagina de Noticias en React",
+                Link = "https://www.nytimes.com/international/",
+                ImagenURL = "/imagenes/nyt.png"
+            },
+            new Proyecto
+            {
+                Titulo = "Reddit",
+                Description = "Red Social para compartir en comunidades",
+                Link = "https://www.reddit.com/",
+                ImagenURL = "/imagenes/reddit.png"
+            },
+            new Proyecto
+            {
+                Titulo = "Steam",
+                Description = "Tienda en linea para comprar videojuegos",
+                Link = "https://store.steampowered.com/",
+                ImagenURL = "/imagenes/steam.png"
+            }
+            };
         }
 
         public IActionResult Privacy()
